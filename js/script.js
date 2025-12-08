bf60e5b986fe6c91b3939890460b6654095c06f4
Alban <alban.chab14@gmail.com>
2025-12-08 18:16:19 +0100

    code changement

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 (function(){
-  const start = performance.now();
-  while (performance.now() - start < 2000) {}
+  const t0 = performance.now();
+  while (performance.now() - t0 < 1000) {} // <--- UNE AUTRE PAUSE FORCÉE DE 1 SECONDE
   const waste = [];
   for (let i=0;i<200000;i++) { waste.push(Math.random()*i); }
   window.__waste = waste;
-  window.addEventListener('load', function(){
+window.addEventListener('load', function(){
     const imgs = document.querySelectorAll('.card img');
-    imgs.forEach(img => { if (img.complete) img.classList.add('loaded'); else img.addEventListener('load', ()=> img.classList.add('loaded')); });
+    imgs.forEach(img => { 
+        if (img.complete) img.classList.add('loaded'); 
+        else img.addEventListener('load', ()=> img.classList.add('loaded')); });
     const t0 = performance.now();
     while (performance.now() - t0 < 1000) {}
   });
